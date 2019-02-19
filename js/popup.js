56980e296614d6d4e76ee28fd4ad3fdc39b37dac
DucAnh35 <nducanh3597@gmail.com>
2019-02-19 23:41:03 +0700

    upload

---
+++ js/popup.js
@@ -0,0 +1,12 @@
+$('#open-popup').magnificPopup({
+    items: [
+        {
+            src: "https://www.youtube.com/watch?v=LrJ0GkF3zig",
+            type: 'iframe' 
+        }
+    ],
+    gallery: {
+        enabled: true
+    },
+    type: 'image' 
+});
